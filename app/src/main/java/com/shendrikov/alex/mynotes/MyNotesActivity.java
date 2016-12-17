74981f08cf8b6896de94cfe0e12727f3b7473729
neophyteinpast <aleksandr.shendrikov@gmail.com>
2016-12-17 21:50:59 +0200

    Add menu

--- app/src/main/java/com/shendrikov/alex/mynotes/MyNotesActivity.java
+++ app/src/main/java/com/shendrikov/alex/mynotes/MyNotesActivity.java
@@ -5,6 +5,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 import com.shendrikov.alex.mynotes.adapters.MyAdapter;
 import com.shendrikov.alex.mynotes.model.Person;
@@ -24,8 +27,6 @@ public class MyNotesActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_notes);
 
-
-
         Toolbar toolbar = (Toolbar) findViewById(R.id.my_toolbar);
         setSupportActionBar(toolbar);
 
@@ -42,5 +43,31 @@ public class MyNotesActivity extends AppCompatActivity {
         // specify an adapter (see also next example)
         mAdapter = new MyAdapter(persons);
         mRecyclerView.setAdapter(mAdapter);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        getMenuInflater().inflate(R.menu.my_note_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch(item.getItemId()) {
+            case R.id.action_settings:
+                Toast.makeText(this, "Settings option clicked", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.action_help:
+                Toast.makeText(this, "Help option clicked", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.action_search:
+                Toast.makeText(this, "Search option clicked", Toast.LENGTH_SHORT).show();
+                break;
+            default:
+                break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
