5a0438bc39aadd48aacecd2f1547875033a33755
neophyteinpast <aleksandr.shendrikov@gmail.com>
2017-01-03 16:18:15 +0200

    Add sharing option

--- app/src/main/java/com/shendrikov/alex/mynotes/activity/EditNotesActivity.java
+++ app/src/main/java/com/shendrikov/alex/mynotes/activity/EditNotesActivity.java
@@ -1,8 +1,13 @@
 package com.shendrikov.alex.mynotes.activity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.SharedElementCallback;
+import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.ShareActionProvider;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.EditText;
 
@@ -16,6 +21,8 @@ import butterknife.ButterKnife;
  */
 
 public class EditNotesActivity extends AppCompatActivity {
+
+    private static final String SHARE_TYPE = "text/plain";
 
     @BindView(R.id.name_edit_text)
     protected EditText mNameEditText;
@@ -36,13 +43,43 @@ public class EditNotesActivity extends AppCompatActivity {
     }
 
     @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.my_note_item_menu, menu);
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
     public boolean onOptionsItemSelected(MenuItem item) {
 
         switch(item.getItemId()) {
             case android.R.id.home:
                 finish();
                 break;
+            case R.id.menu_item_share:
+                shareAction();
+                break;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+     //Call to update the share intent
+
+    private void shareAction() {
+
+        Intent shareIntent = new Intent();
+
+        shareIntent.setAction(Intent.ACTION_SEND);
+        shareIntent.putExtra(Intent.EXTRA_TEXT, prepareNotForSharing());
+        shareIntent.setType(SHARE_TYPE);
+
+        startActivity(shareIntent);
+   }
+
+    private String prepareNotForSharing() {
+        String name = mNameEditText.getText().toString();
+        String surName = mSurNameEditText.getText().toString();
+
+        return getString(R.string.sharing_template, name, surName);
     }
 }
